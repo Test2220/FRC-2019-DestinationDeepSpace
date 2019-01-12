407d0e38ac01501ad05b21de705f88d7d3183df7
dhruvbala03 <dhruvbala@gmail.com>
2019-01-12 14:41:37 -0600

    Created a subsystem for spring hatch panel prototype

---
+++ src/main/java/frc/robot/subsystems/SpringHatchPanelTest.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Subsystem for testing Hatch Panel Protoypes
+ * 
+ * @author Dhruv Balasubramanian
+ * @version 1/12/2019
+ */
+
+public class SpringHatchPanelTest extends Subsystem {
+
+    // Instance variables
+    private DoubleSolenoid piston;
+
+    /**
+     * Constructor that initializes piston object
+     * 
+     * @param forwardChannel: the channel that sets the piston forward
+     * @param reverseChannel: the channel that sets the piston reverse
+     */
+    public SpringHatchPanelTest(int forwardChannel, int reverseChannel) {
+        piston = new DoubleSolenoid(forwardChannel, reverseChannel);
+    }
+
+    /**
+     * Method to set the direction of the pistons.
+     * 
+     * @param direction: direction to set the piston to: 0 for forward, 1 for
+     *        reverse
+     */
+    public void setPiston(int direction) {
+        if (direction == 0)
+            piston.set(DoubleSolenoid.Value.kForward);
+        else if (direction == 1)
+            piston.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    @Override
+    public void initDefaultCommand() {
+
+    }
+}
