4dc751fabc30942360887cb790a021a527125d93
dhruvbala03 <dhruvbala@gmail.com>
2019-02-11 20:52:40 -0600

    muaadified ports & controls for functionality

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -14,9 +14,9 @@ public class RobotMap {
   public static final int INTAKE_LEFT_BOTTOM = 0, INTAKE_LEFT_TOP = 1, INTAKE_RIGHT_BOTTOM = 2, INTAKE_RIGHT_TOP = 3;
 
   /* Cargo Talons */
-  public static final int LEFT_ARM_MASTER = 4;
-  public static final int RIGHT_ARM_SLAVE = 5;
-  public static final int INTAKE = 6;
+  public static final int LEFT_ARM_MASTER = 6;
+  public static final int RIGHT_ARM_SLAVE = 7;
+  public static final int INTAKE = 8;
   
   /* HARLES SUBSYSTEM IDS */
   public static final int THRUSTER_FORWARD = 0;
--- src/main/java/frc/robot/commands/ManipulateCargo.java
+++ src/main/java/frc/robot/commands/ManipulateCargo.java
@@ -12,8 +12,8 @@ public class ManipulateCargo extends Command {
 
     @Override
     protected void execute() {
-        Robot.cargo.spinIntake(Robot.oi.getManipulator().getY(Hand.kLeft));
-        Robot.cargo.moveArm(Robot.oi.getManipulator().getY(Hand.kRight));
+        Robot.cargo.spinIntake(Robot.oi.getManipulator().getY(Hand.kLeft) * 0.75);
+        Robot.cargo.moveArm(Robot.oi.getManipulator().getY(Hand.kRight) * 0.3);
     }
 
     @Override
